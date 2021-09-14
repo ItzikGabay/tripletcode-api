3ec43fb0d43ab9dd7da7676f45d3cf37bf616cf1
Itzik <itzikdevio@gmail.com>
2021-09-14 21:03:02 +0300

    initial commit

--- middleware/database/mongoose/index.js
+++ middleware/database/mongoose/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const { logger } = require('bs-logger');
 
 exports.establish_db_connection = (() => {
-    mongoose.connect('mongodb://localhost:27017/tripletcode', {
+    // mongoose.connect('mongodb://localhost:27017/tripletcode', {
+    mongoose.connect('mongodb+srv://admin:admin2@main.gewhd.mongodb.net/tripletCode?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
         // Disabled because of an error:
